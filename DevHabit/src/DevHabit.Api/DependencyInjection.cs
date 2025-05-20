5452e3d046ecfc08a4ee82c8b3dc1f9a6ffbe594
Regis Lima <regislima.105@gmail.com>
2025-05-19 23:40:47 -0300

    REFACTOR: Cleaniing Program.cs

---
+++ DevHabit/src/DevHabit.Api/DependencyInjection.cs
@@ -0,0 +1,99 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.Database.SortMapping;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
+using DevHabit.Api.Middlewares;
+using DevHabit.Api.Tools;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json.Serialization;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace DevHabit.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddControllers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers(options =>
+        {
+            options.ReturnHttpNotAcceptable = true;
+        })
+        .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver =
+            new CamelCasePropertyNamesContractResolver())
+        .AddXmlSerializerFormatters();
+
+        builder.Services.AddOpenApi();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddErrorHandling(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+            };
+        });
+
+        builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("Database"),
+            npgsqlOptions =>
+            {
+                npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application);
+            });
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+            .WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddNpgsql())
+            .WithMetrics(metrics => metrics
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation())
+            .UseOtlpExporter();
+
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeScopes = true;
+            options.IncludeFormattedMessage = true;
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+        builder.Services.AddTransient<SortMappingProvider>();
+        builder.Services.AddSingleton<ISortMapptingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
+            HabitMappings.SortMapping);
+        builder.Services.AddTransient<DataShapingTool>();
+        
+        return builder;
+    }
+}
--- DevHabit/src/DevHabit.Api/Program.cs
+++ DevHabit/src/DevHabit.Api/Program.cs
@@ -1,71 +1,13 @@
-using DevHabit.Api.Database;
-using DevHabit.Api.Database.SortMapping;
-using DevHabit.Api.DTOs.Habits;
-using DevHabit.Api.Entities;
+using DevHabit.Api;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middlewares;
-using DevHabit.Api.Tools;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json.Serialization;
-using Npgsql;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-})
-.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = 
-    new CamelCasePropertyNamesContractResolver())
-.AddXmlSerializerFormatters();
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-builder.Services.AddOpenApi();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"),
-    npgsqlOptions =>
-    {
-        npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application);
-    });
-});
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-    .WithTracing(tracing => tracing
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddNpgsql())
-    .WithMetrics(metrics => metrics
-        .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation()
-        .AddRuntimeInstrumentation())
-    .UseOtlpExporter();
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options.IncludeScopes = true;
-    options.IncludeFormattedMessage = true;
-});
-
-builder.Services.AddTransient<SortMappingProvider>();
-builder.Services.AddSingleton<ISortMapptingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
-    HabitMappings.SortMapping);
-builder.Services.AddTransient<DataShapingTool>();
+builder
+    .AddControllers()
+    .AddErrorHandling()
+    .AddDatabase()
+    .AddObservability()
+    .AddApplicationServices();
 
 var app = builder.Build();
 
