a49f938d37a136908d4256e6701ffc07c4122674
Regis Lima <regislima.105@gmail.com>
2025-06-08 16:24:35 -0300

    FEAT: Implementing User Registration.

---
+++ DevHabit/src/DevHabit.Api/DTOs/Users/UserMappings.cs
@@ -0,0 +1,16 @@
+﻿using DevHabit.Api.DTOs.Auth;
+using DevHabit.Api.Entities;
+
+namespace DevHabit.Api.DTOs.Users;
+
+public static class UserMappings
+{
+    public static User ToEntity(this RegisterUserDto dto) =>
+        new User
+        {
+            Id = $"u_{Guid.CreateVersion7()}",
+            Name = dto.Name,
+            Email = dto.Email,
+            CreatedAtUtc = DateTime.UtcNow
+        };
+}
